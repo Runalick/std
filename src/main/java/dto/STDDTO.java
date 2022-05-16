cfff309285cc6a4f866f57f6048bd18664486359
BWH <woohyoung2@nate.com>
2022-05-16 14:38:58 +0900

    make STDDTO

---
+++ src/main/java/dto/STDDTO.java
@@ -0,0 +1,62 @@
+package dto;
+
+public class STDDTO {
+	private int id;
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private String date;
+	public STDDTO(int id, String name, int kor, int eng, int math, String date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.date = date;
+	}
+	public STDDTO() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+	
+	
+}
